571534f1d8f9b71ba3ba02ecdc00e271e06a193d
w-g-e <w.g.evans.de@gmail.com>
2024-09-01 16:32:21 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+import os
+
+app = Flask(__name__)
+
+# Define the path where uploaded files will be stored
+UPLOAD_FOLDER = 'uploads'
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Route for the main page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Route to handle file uploads
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file part'
+    file = request.files['file']
+    if file.filename == '':
+        return 'No selected file'
+    if file and file.filename.endswith('.gif'):
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(filepath)
+        return redirect(url_for('uploaded_file', filename=file.filename))
+    return 'File is not a GIF'
+
+# Route to display the uploaded file
+@app.route('/uploads/<filename>') # this will need to be updated with where the files are going to be stored
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
